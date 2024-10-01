fb79646bcae6d3d6a43795a5f23803175910970d
Marcos A. Verdura <mav.unlam@gmail.com>
2024-10-01 20:30:27 -0300

    añadido código de ejemplo.

--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/App.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -26,12 +27,24 @@ fun App() {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                val scope = rememberCoroutineScope()
+                var text by remember { mutableStateOf("Loading") }
+                LaunchedEffect(true) {
+                    scope.launch {
+                        text = try {
+                            Greeting().greeting()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
                 }
+                GreetingView(text = text)
             }
         }
     }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
 }
--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/Greeting.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/Greeting.kt
@@ -1,9 +1,16 @@
 package com.unlam.mav.ktor
 
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
 class Greeting {
     private val platform = getPlatform()
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
+    private val client = HttpClient()
+
+    suspend fun greeting(): String {
+        val response = client.get("https://ktor.io/docs/")
+        return "Hello, ${platform.name}!" + response.bodyAsText()
     }
 }
