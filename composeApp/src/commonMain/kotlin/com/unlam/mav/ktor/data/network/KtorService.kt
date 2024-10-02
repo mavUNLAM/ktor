2b07812b6ad58276d10ab3033a7e78bf6748c8a1
Marcos A. Verdura <mav.unlam@gmail.com>
2024-10-02 01:32:26 -0300

    versión inicial de KtorService.getCharacters.

--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/data/network/KtorService.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/data/network/KtorService.kt
@@ -2,8 +2,15 @@ package com.unlam.mav.ktor.data.network
 
 import com.unlam.mav.ktor.core.MarvelCrypto
 import com.unlam.mav.ktor.data.network.login.LogIngCredentials
+import com.unlam.mav.ktor.data.network.model.KtorResponse
+import com.unlam.mav.ktor.data.network.model.Result
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.datetime.Clock
 import kotlinx.serialization.json.Json
@@ -18,8 +25,13 @@ class KtorService {
             )
         }
     }
+    companion object {
+        private const val LIMIT = "100"
+        private const val CHARACTERS_BASE_URL = "https://gateway.marvel.com/v1/public/characters"
+    }
 
-    suspend fun getCharacter(id: Int, logIngCredentials: LogIngCredentials): String {
+    suspend fun getCharacters(page: Int, logIngCredentials: LogIngCredentials): List<Result> {
+        val offset = (page - 1) * LIMIT.toInt()
         val timestamp = Clock.System.now().toEpochMilliseconds()
         val hash = MarvelCrypto()
             .getHash(
@@ -27,7 +39,17 @@ class KtorService {
                         logIngCredentials.privateKey +
                         logIngCredentials.publicKey
             )
-
-        return ""
+        val response = client.get(CHARACTERS_BASE_URL) {
+            contentType(ContentType.Application.Json)
+            parameter("ts", timestamp.toString())
+            parameter("apikey", logIngCredentials.publicKey)
+            parameter("hash", hash)
+            parameter("orderBy", "name")
+            parameter("limit", LIMIT)
+            parameter("offset", offset)
+        }
+        return response.body<KtorResponse>().data.results
     }
+
+
 }
