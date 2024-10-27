ed59924c8d7a4ba489b94dca301e6aed11b19b9b
Marcos A. Verdura <mav.unlam@gmail.com>
2024-10-27 13:46:06 -0300

    no funciona.

--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galeryscreen/GalleryScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galeryscreen/GalleryScreenViewModel.kt
@@ -3,22 +3,35 @@ package com.unlam.mav.ktor.ui.galeryscreen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.unlam.mav.ktor.data.network.KtorService
-import com.unlam.mav.ktor.data.network.login.LogingCredentials
 import com.unlam.mav.ktor.data.network.model.toMarvelCharacter
+import com.unlam.mav.ktor.domain.UC_Gallery
 import com.unlam.mav.ktor.domain.model.MarvelCharacter
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class GalleryScreenViewModel(
-    private val credentials: LogingCredentials,
-    private val ktorService: KtorService
+    private val ktorService: KtorService,
+    private val useCaseGallery: UC_Gallery,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
     ): ViewModel() {
     private val _state = MutableStateFlow<GalleryScreenState>(GalleryScreenState.Loading)
     val state = _state.asStateFlow()
 
     private var currentPage = 0
     private val currentList = mutableListOf<MarvelCharacter>()
+
+    val marvelList = useCaseGallery
+        .marvelList
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            emptyList()
+        )
 
     init {
         loadCharacters()
@@ -42,6 +55,12 @@ class GalleryScreenViewModel(
             val list: List<MarvelCharacter> = characters.map { it.toMarvelCharacter() }
             currentList.addAll(list)
             _state.value = GalleryScreenState.Success(currentList)
+        }
+    }
+
+    fun loadCharactersV2() {
+        viewModelScope.launch(ioDispatcher) {
+
         }
     }
 }
