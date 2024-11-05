29a8d924f6bc435f4dd82bb59874dd4f583dfb51
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-05 18:34:55 -0300

    no funciona 3

--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/domain/UC_GalleryState.kt
+++
@@ -1,7 +0,0 @@
-package com.unlam.mav.ktor.domain
-
-sealed class UC_GalleryState {
-    data object Loading : UC_GalleryState()
-    data object Success : UC_GalleryState()
-    data class Error(val error: Throwable? = null): UC_GalleryState()
-}
--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galeryscreen/GalleryScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galeryscreen/GalleryScreenViewModel.kt
@@ -2,39 +2,26 @@ package com.unlam.mav.ktor.ui.galeryscreen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.unlam.mav.ktor.data.network.KtorService
-import com.unlam.mav.ktor.data.network.model.toMarvelCharacter
-import com.unlam.mav.ktor.domain.UC_Gallery
-import com.unlam.mav.ktor.domain.model.MarvelCharacter
+import com.unlam.mav.ktor.data.repository.CharacterRepository
+import com.unlam.mav.ktor.data.repository.CharacterRepositoryState
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class GalleryScreenViewModel(
-    private val ktorService: KtorService,
-    private val useCaseGallery: UC_Gallery,
+    private val repository: CharacterRepository,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
     ): ViewModel() {
     private val _state = MutableStateFlow<GalleryScreenState>(GalleryScreenState.Loading)
     val state = _state.asStateFlow()
 
     private var currentPage = 0
-    private val currentList = mutableListOf<MarvelCharacter>()
-
-    val marvelList = useCaseGallery
-        .marvelList
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            emptyList()
-        )
 
     init {
-        loadCharacters()
+        loadCharactersV2()
     }
 
     /**
@@ -44,23 +31,31 @@ class GalleryScreenViewModel(
      *
      * Tiene un problema: va a pedir la siguiente página aunque no haya más.
      */
-    fun loadCharacters() {
-        viewModelScope.launch {
-            currentPage++
-            val characters = ktorService
-                .getCharacters(
-                    page = currentPage
-                    //,logIngCredentials = credentials. cambiarlo en un futuro a una implementación de login
-                )
-            val list: List<MarvelCharacter> = characters.map { it.toMarvelCharacter() }
-            currentList.addAll(list)
-            _state.value = GalleryScreenState.Success(currentList)
-        }
-    }
-
     fun loadCharactersV2() {
         viewModelScope.launch(ioDispatcher) {
+            currentPage++
+            when (val repositoryResult = repository.getCharacters(currentPage)) {
+                is CharacterRepositoryState.Error -> {
+                    val error = repositoryResult.error
+                    _state.update {
+                        GalleryScreenState.Error(error.toString())
+                    }
+                }
+                is CharacterRepositoryState.Success -> {
+                    val list = repositoryResult.characters
 
+                    if(currentPage == 1) {
+                        _state.update {
+                            GalleryScreenState.Success(list)
+                        }
+                    } else {
+                        _state.update {
+                            val oldState = it as GalleryScreenState.Success
+                            GalleryScreenState.Success(oldState.characters + list)
+                        }
+                    }
+                }
+            }
         }
     }
 }
