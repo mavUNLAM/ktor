2fe7dab84021f5dcaccc12d3ecba61ab5275e1c3
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-06 04:03:51 -0300

    muestra detalles animado

--- composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galleryscreen/components/GridComponents.kt
+++ composeApp/src/commonMain/kotlin/com/unlam/mav/ktor/ui/galleryscreen/components/GridComponents.kt
@@ -1,5 +1,6 @@
 package com.unlam.mav.ktor.ui.galleryscreen.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -70,7 +71,7 @@ fun GalleryItem(
                 fontWeight = FontWeight.ExtraBold,
             )
         }
-        if(showDetails) {
+        AnimatedVisibility(showDetails) {
             Text(
                 modifier = Modifier.padding(4.dp),
                 text = character.description,
