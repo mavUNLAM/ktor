58e0b5152a7650e1360dd92c251b254f64704eeb
Marcos A. Verdura <mav.unlam@gmail.com>
2024-11-08 03:52:49 -0300

    ...

--- composeApp/src/androidMain/kotlin/com/unlam/mav/ktor/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/unlam/mav/ktor/MainActivity.kt
@@ -3,29 +3,8 @@ package com.unlam.mav.ktor
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.unlam.mav.ktor.domain.model.MarvelCharacter
-import com.unlam.mav.ktor.ui.galleryscreen.GalleryScreenState
-import com.unlam.mav.ktor.ui.galleryscreen.GalleryScreenViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +22,7 @@ fun AppAndroidPreview() {
     App()
 }
 
+/*
 @Composable
 fun GalleryScreen(
     modifier: Modifier = Modifier,
@@ -172,3 +152,4 @@ fun CharacterItem(
         }
     }
 }
+*/
